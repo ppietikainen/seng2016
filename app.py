deb199983abcc07dd5c029f3189f8e66bfcd84b2
Samuel <samuel@Samuel.local>
2016-02-09 10:28:43 +0200

    Modified app.py to include primes

--- app.py
+++ app.py
@@ -22,14 +22,20 @@ class FizzBuzz():
 
     # Seems to give correct values. Tested with 1 and 2.
     def calc(self, i):
-        if i % 3 == 0 and i % 5 == 0:
+        if i == 1:
+            return i
+        elif i % 3 == 0 and i % 5 == 0:
             return "FizzBuzz"
         elif i % 3 == 0:
             return "Fizz"
         elif i % 5 == 0:
             return "Buzz"
         else:
-            return i
+            for x in range(2, i):
+                if i % x == 0:
+                    return i
+            return "%d is a prime" % i
+
 
 if __name__ == "__main__":
     app = FizzBuzz()
