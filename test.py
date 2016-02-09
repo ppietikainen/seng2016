e16e9525ecf1ca7f86c3e8cf214a38d5a7be6201
Samuel <samuel@Samuel.local>
2016-02-09 09:06:53 +0200

    Fixed app.py

--- test.py
+++ test.py
@@ -10,13 +10,14 @@ class TestSuite(unittest.TestCase):
         app = FizzBuzz()
         self.failIf(app.calc(1) != 1)
 
+
     def test_run(self):
         output = StringIO()
 
         app = FizzBuzz()
         app.run(100, output)
 
-        self.failIf(len(output.getvalue().splitlines()) == 100)
+        self.failIf(len(output.getvalue().splitlines()) != 100)
 
 def main():
     unittest.main()
