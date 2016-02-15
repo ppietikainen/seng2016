15383a233bf7bee603f72e8df111515377931e5b
Janne Körkkö <jkorkko@openmailbox.org>
2016-02-15 13:47:40 +0200

    Added test unit for testing "fizz"

--- test.py
+++ test.py
@@ -8,9 +8,14 @@ class TestSuite(unittest.TestCase):
 
     def test_one(self):
         app = FizzBuzz()
-
-
         self.failIf(app.calc(1) != 1)
+
+    def test_fizz(self):
+        app = FizzBuzz()
+        test_fizz = [3, 6, 9, 12, 18, 21, 24]
+        # Run test
+        for value in test_fizz:
+            self.failIf(app.calc(value) != "Fizz")
 
     def test_run(self):
         output = StringIO()
